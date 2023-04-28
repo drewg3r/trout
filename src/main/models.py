afe0d49bc8e81ec6bdd381bd234b77c0272511d2
Andrew <drewg3r@gmail.com>
2023-04-28 17:48:01 +0200

    Initial migration

--- src/main/models.py
+++ src/main/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class City(models.Model):
+    name = models.CharField(max_length=64)
+
+
 class Route(models.Model):
     name = models.CharField(max_length=64)
 
@@ -12,18 +16,15 @@ class Connection(models.Model):
     route = models.ForeignKey(to=Route, on_delete=models.CASCADE, related_name='connections')
 
 
-class Waypoint(models.Model):
-    station = models.ForeignKey(to=Station, on_delete=models.CASCADE, related_name='waypoints')
-    connection = models.ForeignKey(to=Connection, on_delete=models.CASCADE, related_name='waypoints')
-    trip_time = models.IntegerField()
-    disabled = models.BooleanField(default=False)
-
 class Station(models.Model):
     name = models.CharField(max_length=64)
     city = models.ForeignKey(to=City, on_delete=models.PROTECT, related_name='stations')
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
-    
-class City(models.Model):
-    name = models.CharField(max_length=64)
+
+class Waypoint(models.Model):
+    station = models.ForeignKey(to=Station, on_delete=models.CASCADE, related_name='waypoints')
+    connection = models.ForeignKey(to=Connection, on_delete=models.CASCADE, related_name='waypoints')
+    trip_time = models.IntegerField()
+    disabled = models.BooleanField(default=False)
