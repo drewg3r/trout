7040e4fca27105ff22ce7d0b7b85b0d9d6487ebf
al3nka <kiychenkoak@gmail.com>
2023-05-04 11:29:46 +0200

    Add views for some models

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main.models import City, Station
+from main.models import City, Station, Route, Connection, Waypoint
 
 
 @admin.register(City)
@@ -10,4 +10,24 @@ class CityAdmin(admin.ModelAdmin):
 
 @admin.register(Station)
 class StationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    pass
+
+
+class WaypointInline(admin.TabularInline):
+    model = Waypoint
+    extra = 3
+
+
+@admin.register(Connection)
+class ConnectionAdmin(admin.ModelAdmin):
+    inlines = [WaypointInline]
+
+
+@admin.register(Waypoint)
+class WaypointAdmin(admin.ModelAdmin):
     pass
