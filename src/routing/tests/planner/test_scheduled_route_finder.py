05030b947c8ebfa7d9c1085959805ac3a23df3ef
al3nka <kiychenkoak@gmail.com>
2023-05-05 16:41:40 +0200

    Fix syntax warnings

--- src/routing/tests/planner/test_scheduled_route_finder.py
+++ src/routing/tests/planner/test_scheduled_route_finder.py
@@ -14,14 +14,14 @@ def test_route_with_transfer_options(scheduled_route_finder):
     assert len(route) == 3
     assert route == {
         1: {'arrival_time': datetime(2023, 4, 25, 0, 35),
-              'departure_time': datetime(2023, 4, 25, 0, 34),
-              'connection_id': 2},
+            'departure_time': datetime(2023, 4, 25, 0, 34),
+            'connection_id': 2},
         3: {'arrival_time': datetime(2023, 4, 25, 0, 54),
-              'departure_time': datetime(2023, 4, 25, 0, 47),
-              'connection_id': 2},
+            'departure_time': datetime(2023, 4, 25, 0, 47),
+            'connection_id': 2},
         6: {'arrival_time': datetime(2023, 4, 25, 1, 4),
-              'departure_time': datetime(2023, 4, 25, 0, 55),
-              'connection_id': 2}
+            'departure_time': datetime(2023, 4, 25, 0, 55),
+            'connection_id': 2}
     }
 
 
@@ -29,8 +29,8 @@ def test_route_without_transfer(scheduled_route_finder):
     route = scheduled_route_finder([1, 2], datetime(2023, 4, 24, 16, 0))
     assert len(route) == 1
     assert route == {1: {'arrival_time': datetime(2023, 4, 24, 16, 14),
-                           'departure_time': datetime(2023, 4, 24, 16, 12),
-                           'connection_id': 1}}
+                         'departure_time': datetime(2023, 4, 24, 16, 12),
+                         'connection_id': 1}}
 
 
 def test_not_enough_nodes(scheduled_route_finder):
